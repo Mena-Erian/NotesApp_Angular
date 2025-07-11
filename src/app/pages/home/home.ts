3e775a0cc55c015fa2456064b417269a67a881c4
Mena Erian Farouk <135561286+Mena-Erian@users.noreply.github.com>
2025-07-11 22:34:09 +0300

    Add Note

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,10 +2,16 @@ import { Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { Notes } from '../../core/services/notes/notes';
 import { isPlatformBrowser } from '@angular/common';
 import { INote } from '../../core/interfaces/notes.interface';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
@@ -13,6 +19,12 @@ export class Home implements OnInit {
   private readonly note = inject(Notes);
   private readonly pLATFORM_ID = inject(PLATFORM_ID);
   notesList: INote[] = [];
+
+  addToForm: FormGroup = new FormGroup({
+    title: new FormControl(null, [Validators.required]),
+    content: new FormControl(null, [Validators.required]),
+  });
+
   ngOnInit(): void {
     if (isPlatformBrowser(this.pLATFORM_ID)) {
       this.getUserNotes();
@@ -24,6 +36,18 @@ export class Home implements OnInit {
       next: (res) => {
         console.log(res);
         this.notesList = res.notes;
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
+  }
+
+  addNoteSubmit() {
+    this.note.addNotes(this.addToForm.value).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.getUserNotes();
       },
       error: (err) => {
         console.error(err);
